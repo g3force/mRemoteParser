7722478eb2506c39c4e5dfe29358764cb0569c87
Nicolai Ommer <nicolai.ommer@qaware.de>
2018-07-13 20:32:03 +0200

    Take config file from environment variable

--- mRemoteParser.sh
+++ mRemoteParser.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-confFile=confCons.xml
-command="`go run mRemoteParser.go -f $confFile $@`"
-echo $command
-$command
+debug=1
+
+if [ ! -e "$MREMOTE_CONFIG_FILE" ]; then
+    echo "Set location of config file with the MREMOTE_CONFIG_FILE environment variable"
+    exit 1
+fi
+
+input="mRemoteParser -f $MREMOTE_CONFIG_FILE $@"
+[[ $debug == 1 ]] && echo "Input: $input"
+command="`$input`"
+[[ $debug == 1 ]] && echo "Command: $command"
+eval $command
